c3ae5aa2d0498bdfb9c77f727a7e68699a556fb1
drewtchrist <andrewtaylorchristiansen@gmail.com>
2021-01-29 17:24:57 -0700

    Forgot to add sensor classes to impl.__init__.py

--- avista_sensors/impl/__init__.py
+++ avista_sensors/impl/__init__.py
@@ -2,3 +2,8 @@
 
 import avista_sensors.impl.random_processor
 import avista_sensors.impl.simulated_processor
+import avista_sensors.impl.vibration_processor
+import avista_sensors.impl.sound_processor
+import avista_sensors.impl.dht_processor
+import avista_sensors.impl.irbeam_processor
+import avista_sensors.impl.irtemp_processor

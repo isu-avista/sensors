f5d778c041da4146a076af7d06d05b1e46833e90
Isaac Griffith <grifisaa@isu.edu>
2021-02-16 02:37:47 -0700

    updated documentation, ready for release 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="avista-sensors",
-    version="0.5.0",
+    version="0.5.1",
     author="Isaac Griffith",
     author_email="grifisaa@isu.edu",
     description="",
